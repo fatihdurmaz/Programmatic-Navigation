1b5c5ea793819051513f75a02c21714e70c6e167
Fatih DURMAZ <fatih.durmaz@outlook.com>
2024-07-31 10:07:20 +0300

    Initial Commit

---
+++ Programmatic Navigation/Programmatic_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Programmatic_NavigationApp.swift
+//  Programmatic Navigation
+//
+//  Created by Fatih Durmaz on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct Programmatic_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
